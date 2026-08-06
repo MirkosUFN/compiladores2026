6cb827a119281611a3ac01dba8a61fcd63257940
Nyx barros <nyxbarros6@proton.me>
2026-08-06 10:34:28 -0300

    Teste do programa para remover comentarios

--- preprocessamento/Nyx Barros/remover comentarios/testar.py
+++ preprocessamento/Nyx Barros/remover comentarios/testar.py
@@ -3,5 +3,5 @@ import remover_comentarios
 arquivos = ['teste1', 'teste2', 'teste3', 'teste4', 'teste5']
 
 for i in arquivos:
-    pasta = 'testes/remover_comentario/'
-    remover_comentarios.processar_arquivo(pasta+i, pasta+i+'-resposta')
+    arquivo = 'testes/remover_comentario/'+i
+    remover_comentarios.processar_arquivo(arquivo, arquivo+'-resposta')
