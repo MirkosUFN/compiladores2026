1fa2786691bb518722ab7b6aff4524997fd2ea85
Nyx barros <nyxbarros6@proton.me>
2026-08-21 11:15:08 -0300

    AFD que gera tabela CSV feito por Nyx Barros

---
+++ AnaliseLexica/AFD/implementacoes/NyxBarros/teste/codigo.py
@@ -0,0 +1,10 @@
+import os
+
+from codigo.tabela_simbolos import TabelaSimbolos
+
+arquivo_config_afd = 'config/configAfd.md' if os.path.exists('config/configAfd.md') else 'config/configAfd.txt'
+arquivo_teste = 'teste/teste.txt'
+
+# Criar a tabela e analisar automaticamente
+tabela_simbolos = TabelaSimbolos(arquivo_config_afd, arquivo_teste)
+tabela_simbolos.gerar_csv()
