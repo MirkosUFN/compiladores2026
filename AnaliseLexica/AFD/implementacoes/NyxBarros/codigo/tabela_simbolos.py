9f7cd168afbbd008e650279cfd4eb8ea3aa1a68f
Nyx barros <nyxbarros6@proton.me>
2026-08-21 11:51:28 -0300

    programa gera arquivo json

--- AnaliseLexica/AFD/implementacoes/NyxBarros/codigo/tabela_simbolos.py
+++ AnaliseLexica/AFD/implementacoes/NyxBarros/codigo/tabela_simbolos.py
@@ -1,6 +1,6 @@
 from codigo.afd import AFD
 
-import csv
+import csv, json
 
 class TabelaSimbolos:
     def __init__(self, arquivo_config_afd: str, arquivo_para_analizar=None) -> None:
@@ -30,9 +30,15 @@ class TabelaSimbolos:
                 cont_linha += 1
         return self.analise
 
-    def gerar_csv(self, arquivo_final : str|None = None):
+    def gerar_arquivo_csv(self, arquivo_final : str|None = None):
         arquivo_final = arquivo_final or 'tabela_de_simbolos.csv'
         with open(arquivo_final, 'w') as arquivo_tabela:
             arquivo_tabela.write(','.join(self.analise[0].keys())+'\n')
             for linha_tabela in self.analise:
                 arquivo_tabela.write(','.join(str(v) for v in linha_tabela.values())+'\n')
+
+    def gerar_arquivo_json(self, arquivo_final : str|None = None):
+        arquivo_final = arquivo_final or 'tabela_de_simbolos.json'
+        with open(arquivo_final, 'w') as arquivo_json:
+            json.dump(self.analise, arquivo_json, indent=4)
+
--- AnaliseLexica/AFD/implementacoes/NyxBarros/teste/codigo.py
+++ AnaliseLexica/AFD/implementacoes/NyxBarros/teste/codigo.py
@@ -7,4 +7,5 @@ arquivo_teste = 'teste/teste.txt'
 
 # Criar a tabela e analisar automaticamente
 tabela_simbolos = TabelaSimbolos(arquivo_config_afd, arquivo_teste)
-tabela_simbolos.gerar_csv()
+tabela_simbolos.gerar_arquivo_csv()
+tabela_simbolos.gerar_arquivo_json()
