117d90170c62df26a497ad29cdf52507e577be92
Nyx barros <nyxbarros6@proton.me>
2026-08-27 15:52:40 -0300

    aceitar virgula, ponto e virgula, atribuiçao e comparaço

--- AnaliseLexica/AFD/implementacoes/NyxBarros/teste/codigo.py
+++ AnaliseLexica/AFD/implementacoes/NyxBarros/teste/codigo.py
@@ -1,11 +1,13 @@
 import os
 
-from codigo.tabela_simbolos import TabelaSimbolos
+from codigo.afd import AFD
 
 arquivo_config_afd = 'config/configAfd.md' if os.path.exists('config/configAfd.md') else 'config/configAfd.txt'
 arquivo_teste = 'teste/teste.txt'
 
 # Criar a tabela e analisar automaticamente
-tabela_simbolos = TabelaSimbolos(arquivo_config_afd, arquivo_teste)
+tabela_simbolos = AFD(arquivo_config_afd)
+tabela_simbolos.analisar_arquivo(arquivo_teste)
 tabela_simbolos.gerar_arquivo_csv()
 tabela_simbolos.gerar_arquivo_json()
+tabela_simbolos.gerar_arquivo_mk()
---
+++ AnaliseLexica/AFD/implementacoes/NyxBarros/teste/tabela_de_simbolos.md
@@ -0,0 +1,10 @@
+| ID | token | tipo          | linha | coluna |
+|----|-------|---------------|-------|--------|
+|  1 | 54    | INTEIRO       |     0 |      0 |
+|  2 | 5     | INTEIRO       |     1 |      0 |
+|  3 | 19.4  | FRACIONADO    |     2 |      0 |
+|  4 | int   | NOME_VARIAVEL |     4 |      0 |
+|  5 | x     | NOME_VARIAVEL |     4 |      4 |
+|  6 | =     | ATRIUCAO      |     4 |      6 |
+|  7 | 0     | INTEIRO       |     4 |      8 |
+|  8 | ;     | PONTO_VIRGULA |     4 |     10 |
